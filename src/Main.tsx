dc1fb07a695274f68d3b6ef601eed2b58ceb6398
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2020-11-01 16:14:03 +0530

    basic page ready

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import "styles/main.css";
 
 function Main() {
+  const [longUrl, setLongUrl] = useState("");
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(longUrl);
+  };
+
   return (
     <div className="main">
-      <h1>hello</h1>
+      <div className="container">
+        <h3>Easy URL Shortner</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            placeholder="Enter Long URL"
+            value={longUrl}
+            onChange={(e) => setLongUrl(e.target.value)}
+          />
+        </form>
+      </div>
     </div>
   );
 }
